4ce269508aded488bde3d8030bd75bac565ffbda
Nameless <zwigith@gmail.com>
2017-06-23 07:32:58 -0400

    Added in the gb2 strings

--- extras/translations/strings_jp.pl
+++ extras/translations/strings_jp.pl
@@ -1,76 +1,79 @@
 use encoding "shift-jis";
 
-use constant S_HOME => '�z�[��';
-use constant S_ADMIN => '�Ǘ��p';
-use constant S_RETURN => '�f���ɖ߂�';
-use constant S_POSTING => '���X���M���[�h';
+use constant S_HOME => 'ホーム';
+use constant S_ADMIN => '管理用';
+use constant S_RETURN => '掲示板に戻る';
+use constant S_POSTING => 'レス送信モード';
 
-use constant S_NAME => '���Ȃ܂�';
+use constant S_NAME => 'おなまえ';
 use constant S_EMAIL => 'E-mail';
-use constant S_SUBJECT => '��@�@��';
-use constant S_SUBMIT => '���M����';
-use constant S_COMMENT => '�R�����g';
-use constant S_UPLOADFILE => '�Y�tFile';
-use constant S_NOFILE => '�摜�Ȃ�';
-use constant S_CAPTCHA => '����';
-use constant S_PARENT => '�X��';
-use constant S_DELPASS => '�폜�L�[';
-use constant S_DELEXPL => '(�L���̍폜�p�B�p������8�����ȓ�)';
+use constant S_SUBJECT => '題　　名';
+use constant S_SUBMIT => '送信する';
+use constant S_COMMENT => 'コメント';
+use constant S_UPLOADFILE => '添付File';
+use constant S_NOFILE => '画像なし';
+use constant S_BACKTO => 'Return to';
+use constant S_REBOARD => 'board';
+use constant S_RETHREAD => 'thread';
+use constant S_CAPTCHA => '検証';
+use constant S_PARENT => 'スレ';
+use constant S_DELPASS => '削除キー';
+use constant S_DELEXPL => '(記事の削除用。英数字で8文字以内)';
 use constant S_SPAMTRAP => 'Leave these fields empty (spam trap): ';
 
-use constant S_THUMB => '�T���l�C����\�����Ă��܂�.�N���b�N����ƌ��̃T�C�Y��\�����܂�.';
+use constant S_THUMB => 'サムネイルを表示しています.クリックすると元のサイズを表示します.';
 use constant S_HIDDEN => 'Image reply hidden, click name for full image.';
 use constant S_NOTHUMB => 'No<br />thumbnail';
-use constant S_PICNAME => '�摜�^�C�g���F';
-use constant S_REPLY => '�ԐM';
-use constant S_OLD => '���̃X���͌Â��̂ŁA�������������܂��B';
-use constant S_ABBR => '���X%d���ȗ��B�S�ēǂނɂ͕ԐM�{�^���������Ă��������B';
-use constant S_ABBRIMG => '���X%d,%d���ȗ��B�S�ēǂނɂ͕ԐM�{�^���������Ă��������B';
-use constant S_ABBRTEXT => '�ȗ�����܂����E�E�S�Ă�ǂނɂ�<a href="%s">����</a>�������Ă�������';
+use constant S_PICNAME => '画像タイトル：';
+use constant S_REPLY => '返信';
+use constant S_OLD => 'このスレは古いので、もうすぐ消えます。';
+use constant S_ABBR => 'レス%d件省略。全て読むには返信ボタンを押してください。';
+use constant S_ABBRIMG => 'レス%d,%d件省略。全て読むには返信ボタンを押してください。';
+use constant S_ABBRTEXT => '省略されました・・全てを読むには<a href="%s">ここ</a>を押してください';
 
-use constant S_REPDEL => '�y�L���폜�z';
-use constant S_DELPICONLY => '�摜��������';
-use constant S_DELKEY => '�폜�L�[';
-use constant S_DELETE => '�폜';
+use constant S_REPDEL => '【記事削除】';
+use constant S_DELPICONLY => '画像だけ消す';
+use constant S_DELKEY => '削除キー';
+use constant S_DELETE => '削除';
 
-use constant S_PREV => '�O�̃y�[�W';
-use constant S_FIRSTPG => '�ŏ��̃y�[�W';
-use constant S_NEXT => '���̃y�[�W';
-use constant S_LASTPG => '�Ō�̃y�[�W';
+use constant S_PREV => '前のページ';
+use constant S_FIRSTPG => '最初のページ';
+use constant S_NEXT => '次のページ';
+use constant S_LASTPG => '最後のページ';
 
-use constant S_WEEKDAYS => ('��','��','��','��','��','��','�y');
+use constant S_WEEKDAYS => ('日','月','火','水','木','金','土');
 
-use constant S_MANARET => '�f���ɖ߂�';
-use constant S_MANAMODE => '�Ǘ����[�h';
+use constant S_MANARET => '掲示板に戻る';
+use constant S_MANAMODE => '管理モード';
 
 use constant S_MANALOGIN => 'Manager Login';
 use constant S_ADMINPASS => 'Admin password:';
 
-use constant S_MANAPANEL => '�L���폜';
+use constant S_MANAPANEL => '記事削除';
 use constant S_MANABANS => 'Bans';
 use constant S_MANAPROXY => 'Proxy Panel';
-use constant S_MANASPAM => '�X�p��';
+use constant S_MANASPAM => 'スパム';
 use constant S_MANASQLDUMP => 'SQL Dump';
 use constant S_MANASQLINT=> 'SQL Interface';
-use constant S_MANAPOST => '�Ǘ��l���e';
-use constant S_MANAREBUILD => '�L���b�V���̍č\�z';
+use constant S_MANAPOST => '管理人投稿';
+use constant S_MANAREBUILD => 'キャッシュの再構築';
 use constant S_MANANUKE => 'Nuke board';
 use constant S_MANALOGOUT => 'Log out';									# 
 use constant S_MANASAVE => 'Remember me on this computer';				# Defines Label for the login cookie checbox
-use constant S_MANASUB => ' �F��';
+use constant S_MANASUB => ' 認証';
 
-use constant S_NOTAGS => '�^�O�������܂�';
+use constant S_NOTAGS => 'タグがつかえます';
 
 use constant S_MPDELETEIP => 'Delete all';
-use constant S_MPDELETE => '�폜����';
+use constant S_MPDELETE => '削除する';
 use constant S_MPARCHIVE => 'Archive';
-use constant S_MPRESET => '���Z�b�g';
-use constant S_MPONLYPIC => '�摜��������';
+use constant S_MPRESET => 'リセット';
+use constant S_MPONLYPIC => '画像だけ消す';
 use constant S_MPDELETEALL => 'Del all';
 use constant S_MPBAN => 'Ban';
 use constant S_MPTABLE => '<th>Post No.</th><th>Time</th><th>Subject</th>'.
                           '<th>Name</th><th>Comment</th><th>IP</th>';
-use constant S_IMGSPACEUSAGE => '�y �摜�f�[�^���v : <b>%d</b> KB �z';
+use constant S_IMGSPACEUSAGE => '【 画像データ合計 : <b>%d</b> KB 】';
 
 use constant S_BANTABLE => '<th>Type</th><th>Value</th><th>Comment</th><th>Action</th>';
 use constant S_BANIPLABEL => 'IP';
@@ -103,46 +106,46 @@ use constant S_SPAMRESET => 'Restore';
 use constant S_SQLNUKE => 'Nuke password:';
 use constant S_SQLEXECUTE => 'Execute';
 
-use constant S_TOOBIG => '�A�b�v���[�h�Ɏ��s���܂���<br />�T�C�Y���傫�����܂�<br />'.MAX_KB.'K�o�C�g�܂�';
-use constant S_TOOBIGORNONE => '�A�b�v���[�h�Ɏ��s���܂���<br />�摜�T�C�Y���傫�����邩�A<br />�܂��͉摜������܂���B';
-use constant S_REPORTERR => '�Y���L�����݂���܂���';
-use constant S_UPFAIL => '�A�b�v���[�h�Ɏ��s���܂���<br />�T�[�o���T�|�[�g���Ă��Ȃ��\��������܂�';
-use constant S_NOREC => '�A�b�v���[�h�Ɏ��s���܂���<br />�摜�t�@�C���ȊO�͎󂯕t���܂���';
+use constant S_TOOBIG => 'アップロードに失敗しました<br />サイズが大きすぎます<br />'.MAX_KB.'Kバイトまで';
+use constant S_TOOBIGORNONE => 'アップロードに失敗しました<br />画像サイズが大きすぎるか、<br />または画像がありません。';
+use constant S_REPORTERR => '該当記事がみつかりません';
+use constant S_UPFAIL => 'アップロードに失敗しました<br />サーバがサポートしていない可能性があります';
+use constant S_NOREC => 'アップロードに失敗しました<br />画像ファイル以外は受け付けません';
 use constant S_NOCAPTCHA => 'Error: No verification code on record - it probably timed out.';
-use constant S_BADCAPTCHA => '�s���Ȍ��؃R�[�h�����͂���܂���';
+use constant S_BADCAPTCHA => '不正な検証コードが入力されました';
 use constant S_BADFORMAT => 'Error: File format not supported.';
-use constant S_STRREF => '���₳��܂���(str)';
-use constant S_UNJUST => '�s���ȓ��e�����Ȃ��ŉ�����(post)';
-use constant S_NOPIC => '�摜������܂���';
-use constant S_NOTEXT => '���������ĉ�����';
-use constant S_TOOLONG => '�{�����������܂����I';
-use constant S_NOTALLOWED => '�Ǘ��l�ȊO�͓��e�ł��܂���';
-use constant S_UNUSUAL => '�ُ�ł�';
-use constant S_BADHOST => '���₳��܂���(host)';
+use constant S_STRREF => '拒絶されました(str)';
+use constant S_UNJUST => '不正な投稿をしないで下さい(post)';
+use constant S_NOPIC => '画像がありません';
+use constant S_NOTEXT => '何か書いて下さい';
+use constant S_TOOLONG => '本文が長すぎますっ！';
+use constant S_NOTALLOWED => '管理人以外は投稿できません';
+use constant S_UNUSUAL => '異常です';
+use constant S_BADHOST => '拒絶されました(host)';
 use constant S_BADHOSTPROXY => 'Error: Proxy is banned for being open.';				# Returns error for banned proxy ($badip string)
-use constant S_RENZOKU => '�A�����e�͂������΂炭���Ԃ�u���Ă��炨�肢�v���܂�';
-use constant S_RENZOKU2 => '�摜�A�����e�͂������΂炭���Ԃ�u���Ă��炨�肢�v���܂�';
-use constant S_RENZOKU3 => '�A�����e�͂������΂炭���Ԃ�u���Ă��炨�肢�v���܂�';
-use constant S_PROXY => '�d�q�q�n�q�I�@���J�o�q�n�w�x�K�����I�I(%d)';
-use constant S_DUPE => '�A�b�v���[�h�Ɏ��s���܂���<br />�����摜������܂� (<a href="%s">link</a>)';
+use constant S_RENZOKU => '連続投稿はもうしばらく時間を置いてからお願い致します';
+use constant S_RENZOKU2 => '画像連続投稿はもうしばらく時間を置いてからお願い致します';
+use constant S_RENZOKU3 => '連続投稿はもうしばらく時間を置いてからお願い致します';
+use constant S_PROXY => 'ＥＲＲＯＲ！　公開ＰＲＯＸＹ規制中！！(%d)';
+use constant S_DUPE => 'アップロードに失敗しました<br />同じ画像があります (<a href="%s">link</a>)';
 use constant S_DUPENAME => 'Error: A file with the same name already exists.';
-use constant S_NOTHREADERR => '�X���b�h������܂���';
-use constant S_BADDELPASS => '�Y���L����������Ȃ����p�X���[�h���Ԉ���Ă��܂�';
-use constant S_WRONGPASS => '�p�X���[�h���Ⴂ�܂�';
-use constant S_NOTWRITE => '�������܂���<br />';
-use constant S_SPAM => '�X�p���𓊍e���Ȃ��ŉ�����';					# Returns error when detecting spam
+use constant S_NOTHREADERR => 'スレッドがありません';
+use constant S_BADDELPASS => '該当記事が見つからないかパスワードが間違っています';
+use constant S_WRONGPASS => 'パスワードが違います';
+use constant S_NOTWRITE => 'を書けません<br />';
+use constant S_SPAM => 'スパムを投稿しないで下さい';					# Returns error when detecting spam
 
-use constant S_SQLCONF => '�ڑ����s';
-use constant S_SQLFAIL => 'sql���s';
+use constant S_SQLCONF => '接続失敗';
+use constant S_SQLFAIL => 'sql失敗';
 
 use constant S_REDIR => 'If the redirect didn\'t work, please choose one of the following mirrors:';    # Redir message for html in REDIR_DIR
 
-#define(S_ANONAME, '������');
-#define(S_ANOTEXT, '�{���Ȃ�');
-#define(S_ANOTITLE, '����');
-#use constant S_MPTITLE => '�폜�������L���̃`�F�b�N�{�b�N�X�Ƀ`�F�b�N�����A�폜�{�^���������ĉ������B';
-#define(S_MDTABLE1, '<th>�폜</th><th>�L��No</th><th>���e��</th><th>�薼</th>');
-#define(S_MDTABLE2, '<th>���e��</th><th>�R�����g</th><th>�z�X�g��</th><th>�Y�t<br />(Bytes)</th><th>md5</th>');
+#define(S_ANONAME, '名無し');
+#define(S_ANOTEXT, '本文なし');
+#define(S_ANOTITLE, '無題');
+#use constant S_MPTITLE => '削除したい記事のチェックボックスにチェックを入れ、削除ボタンを押して下さい。';
+#define(S_MDTABLE1, '<th>削除</th><th>記事No</th><th>投稿日</th><th>題名</th>');
+#define(S_MDTABLE2, '<th>投稿者</th><th>コメント</th><th>ホスト名</th><th>添付<br />(Bytes)</th><th>md5</th>');
 
 no encoding;
 1;
